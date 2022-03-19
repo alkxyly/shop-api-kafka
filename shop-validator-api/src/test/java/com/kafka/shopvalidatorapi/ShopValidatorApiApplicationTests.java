f15f2b6d888c1aa227d4db9b1a744586c3132916
alkxyly <alkxyly@hotmail.com>
2022-03-19 19:23:43 -0300

    microservico para validar uma compra

---
+++ shop-validator-api/src/test/java/com/kafka/shopvalidatorapi/ShopValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.shopvalidatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopValidatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
